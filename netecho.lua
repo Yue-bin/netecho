50df59364f9605dc439d8b9daa269237a1a312a6
Yue-bin <yuebinjiang@qq.com>
2024-11-04 23:20:29 +0800

    ✨ feat(arg): 增加对传入参数的判断

--- netecho.lua
+++ netecho.lua
@@ -2,44 +2,48 @@
 
 local socket = require("socket")
 local url = require("socket.url")
-
-io.write("running in ", arg[1], " mode, listening on port ", arg[2], "\n")
-if arg[1] == "udp" then
-    -- udp
-    local udp = socket.udp()
-    udp:setsockname("*", arg[2])
-    udp:settimeout(0)
-    while true do
-        local data, ip, port = udp:receivefrom()
-        if data then
-            data = url.escape(data)
-            print("received: \"" .. data .. "\" from " .. ip .. ":" .. port)
-            data = "echo: \"" .. data .. "\" to " .. ip .. ":" .. port .. "\n"
-            print(data)
-            udp:sendto(data, ip, port)
-        end
-        socket.sleep(0.01)
-    end
-elseif arg[1] == "tcp" then
-    -- tcp
-    local tcp = socket.tcp()
-    tcp:bind("*", arg[2])
-    tcp:listen()
-    while true do
-        local client = tcp:accept()
-        local ip, port, nettype = client:getpeername()
-        print("connected with " .. ip .. ":" .. port .. " using " .. nettype)
-        client:send("connection established, you are " .. ip .. ":" .. port .. " using " .. nettype .. "\n")
-        repeat
-            local data = client:receive()
+arg[2] = tonumber(arg[2]) or 0
+if (arg[1] == "udp" or arg[1] == "tcp") and (arg[2] > 0 and arg[2] < 65536) then
+    io.write("running in ", arg[1], " mode, listening on port ", arg[2], "\n")
+    if arg[1] == "udp" then
+        -- udp
+        local udp = socket.udp()
+        udp:setsockname("*", arg[2])
+        udp:settimeout(0)
+        while true do
+            local data, ip, port = udp:receivefrom()
             if data then
-                print("received: \"" .. data .. "\"")
-                data = "echo: \"" .. data .. "\"\n"
+                data = url.escape(data)
+                print("received: \"" .. data .. "\" from " .. ip .. ":" .. port)
+                data = "echo: \"" .. data .. "\" to " .. ip .. ":" .. port .. "\n"
                 print(data)
-                client:send(data)
+                udp:sendto(data, ip, port)
             end
-        until not data
-        print("closing connection\n")
-        client:close()
+            socket.sleep(0.01)
+        end
+    elseif arg[1] == "tcp" then
+        -- tcp
+        local tcp = socket.tcp()
+        tcp:bind("*", arg[2])
+        tcp:listen()
+        while true do
+            local client = tcp:accept()
+            local ip, port, nettype = client:getpeername()
+            print("connected with " .. ip .. ":" .. port .. " using " .. nettype)
+            client:send("connection established, you are " .. ip .. ":" .. port .. " using " .. nettype .. "\n")
+            repeat
+                local data = client:receive()
+                if data then
+                    print("received: \"" .. data .. "\"")
+                    data = "echo: \"" .. data .. "\"\n"
+                    print(data)
+                    client:send(data)
+                end
+            until not data
+            print("closing connection\n")
+            client:close()
+        end
     end
+else
+    io.stderr:write("usage: ", arg[0], " [tcp|udp] port\n")
 end
